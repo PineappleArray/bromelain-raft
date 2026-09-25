700dcafe4eebb95129929c0e411cdcb39cb8bf2e
PineappleArray <originalpineapple02@gmail.com>
2026-09-24 20:26:57 -0400

    added some status errors

--- rpc.go
+++ rpc.go
@@ -1,12 +1,29 @@
 package bromelainraft
 
-type rpcResponse struct {
-	e        error
-	response Response
-}
+const voteEndPoint = "/raft/request-vote"
+const AppendEndPoint = "/raft/append-entries"
+
+type StatusCode string
+
+const (
+	badrequest StatusCode = "400 Bad Request"
+	notfound   StatusCode = "404 Not Found"
+	invalid    StatusCode = "405"
+	unexpected StatusCode = "500 Internal Server Error"
+	notready   StatusCode = "503 Service Unavailable"
+)
+
+type PayloadError string
+
+const (
+	ok PayloadError = "200 OK"
+)
 
 type Response struct {
-	status int
+	status           StatusCode
+	playload         PayloadError
+	voteGranted      bool
+	successfulAppend bool
 }
 
 type VoteRequest struct {
@@ -25,10 +42,10 @@ type AppendRequest struct {
 	recipientID string
 }
 
-func (rpc *Rpc) requestVote(request VoteRequest) {
-
+func (rpc *Rpc) requestVote(request VoteRequest) Response {
+	return nil
 }
 
-func (rpc *Rpc) AppendEntry(resquest AppendRequest) {
-
+func (rpc *Rpc) AppendEntry(request AppendRequest) Response {
+	return nil
 }
